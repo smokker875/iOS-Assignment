bfe080e231e66d67bc45a7b5e7ad664813933bba
Andy Nie <andynie@AndydeMacBook-Pro.local>
2017-08-16 16:50:57 +1000

    Initial Commit

---
+++ cinebooking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cinebooking
+//
+//  Created by Andy Nie on 16/8/17.
+//  Copyright © 2017 Andy Nie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
